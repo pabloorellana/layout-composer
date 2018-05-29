7156e2f1cb223823980d53a863dab026b54b0445
pablo <pablo.orellana.torrez@gmail.com>
2018-05-29 19:38:49 -0400

    Fixed bug when moving widget to their original places Fixed bug when moving widget to random places Fixed bug when moving widget from grid to bar Fixed bug whe moving a sw from the services area to other areas Fixed bug when moving widget to service-area

--- src/components/WidgetsPanel.vue
+++ src/components/WidgetsPanel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="widgets-panel">
-    <div id="source" class="container">
+    <div id="widget-source" class="container">
       <room></room>
       <rm-server></rm-server>
     </div>
@@ -15,8 +15,18 @@ import WidgetsMap from '@/plugins/room-manager/WidgetsMap';
 import WidgetFactoryMap from '@/plugins/room-manager/WidgetFactoryMap';
 
 const drake = dragula({
-  copy(el, source) {
-    return $(source).is('#source')
+  revertOnSpill: true,
+  copy(el, {id: sourceId}) {
+    return sourceId === 'widget-source';
+  },
+  accepts(el, {id: targetId}, {id: sourceId}) {
+    const isServiceWidget = $(el).hasClass("no-layout");
+    return !(
+      (sourceId === 'services-container' && targetId !== 'delete-area') ||
+      (sourceId !== 'widget-source' && targetId === 'services-container') ||
+      (!isServiceWidget && targetId === 'services-container') ||
+      targetId === 'widget-source'
+    );
   }
 });
 
@@ -30,18 +40,21 @@ export default {
   },
   mounted () {
     drake.containers.push(...this.getDrakeContainers());
-    drake.on('drop', (el, target, source) => {
-      const targetId = target.id;
-      const cameFromBar = $(source).is('#source');
+    drake.on('drop', (el, {id: targetId}, {id: sourceId}) => {
+      // TODO: in order to recognize the element that was dropped into
+      // the table, the element has to be wraped in a "div" containing
+      // a class with a name mapped in WidgetsMap in order to
+      // get the data that this component should bind in the store
+      const elementType = this.getElementType(el);
 
       // If widget is dragged to the delete area, remove it
       if (targetId === 'delete-area') {
-        return this.deleteWidget(el, source);
+        return this.deleteWidget(el, sourceId);
       }
 
       // If widget was not dragged from the widget bar, then update its position
-      if (!cameFromBar) {
-        return this.updateWidgetLocation(source.id, targetId);
+      if (sourceId !== 'widget-source') {
+        return this.updateWidgetLocation(sourceId, targetId);
       }
 
       // If no prior condition is met, then it should be a new widget
@@ -52,12 +65,12 @@ export default {
 
       //TODO, "service" type widgets should have the "no-layout" class in their
       //main container in order to be recognized
-      const isInvisibleWidget = $(el).hasClass("no-layout");
-      if (isInvisibleWidget) {
-        return this.addServiceWidget(el);
+      const isServiceWidget = $(el).hasClass("no-layout");
+      if (isServiceWidget) {
+        return this.addServiceWidget(elementType);
       }
 
-      this.addNewWidGet(el, targetId);
+      this.addNewWidGet(elementType, targetId);
     });
   },
   methods: {
@@ -68,12 +81,7 @@ export default {
       'setSelectedWidget',
       'deleteContent'
     ]),
-    addServiceWidget(element) {
-      // TODO: in order to recognize the element that was dropped into
-      // the table, the element has to be wraped in a "div" containing
-      // a class with a name mapped in WidgetsMap in order to
-      // get the data that this component should bind in the store
-      const elementType = this.getElementType(element);
+    addServiceWidget(elementType) {
       const widgetModel = WidgetsMap[elementType]();
 
       this.setSelectedWidget(widgetModel);
@@ -81,8 +89,7 @@ export default {
       //const { content: widgetProps } = this.contentByCellId(targetId)
       this.renderWidget($(`#services-container`)[0], { type: elementType, props: widgetModel })
     },
-    addNewWidGet(element, targetId) {
-      const elementType = this.getElementType(element);
+    addNewWidGet(elementType, targetId) {
       const widgetModel = WidgetsMap[elementType]();
 
       this.setContent({ targetId, content: widgetModel });
@@ -100,12 +107,12 @@ export default {
       const {content} = this.contentByCellId(targetId);
       this.setSelectedWidget(content);
     },
-    deleteWidget(element, source) {
-      this.deleteContent(source.id);
+    deleteWidget(element, sourceId) {
+      this.deleteContent(sourceId);
       $(element).remove();
     },
     getDrakeContainers() {
-      const [source] = $('#source');
+      const [source] = $('#widget-source');
       const targets = $('.copy-target');
       return [source, ...targets];
     },
